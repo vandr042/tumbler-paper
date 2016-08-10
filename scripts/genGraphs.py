a44dbf26ece3941cb1111db4596f0b8326fe8fd2
pendgaft <m.schuchard@gmail.com>
2016-08-10 11:40:01 -0400

    minor tweaks to how graphs are made

--- scripts/genGraphs.py
+++ scripts/genGraphs.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import linkStats as ls
 
 file = "../parsedData/plvStats.txt"
 
-#ls.FPvsVP(file)
-#ls.ASvsVPvsCS(file)
+ls.FPvsVP(file)
+ls.ASvsVPvsCS(file)
 ls.TPvsVPvsCS(file)
--- scripts/linkStats.py
+++ scripts/linkStats.py
@@ -15,6 +15,7 @@ def put(xvals, vp):
 def FPvsVP(filename):
     f = open(filename, 'r')
 
+    curFig = plt.figure()
     headers = f.readline()
     xvals = [0]
     yvals = [0.0]
@@ -24,17 +25,18 @@ def FPvsVP(filename):
         i = put(xvals,int(arr[0]))
         yvals.insert(i,float(arr[1]))
         
-    plt.plot(xvals,yvals, label="c1")
+    plt.plot(xvals,yvals, label="c1", lw = 5.0, figure=curFig)
 
-    plt.legend(bbox_to_anchor=(0.,1.02,1.,.102), loc=2, ncol=5, mode = "expand", borderaxespad=0)
+    #plt.legend(bbox_to_anchor=(0.,1.02,1.,.102), loc=2, ncol=5, mode = "expand", borderaxespad=0)
     plt.xlabel("Vantage Points")
     plt.ylabel("First Peer ID Accuracy")
-    plt.savefig("FPvsVP.png")
+    curFig.savefig("../plots/FPvsVP.pdf", format="pdf")
     
 def ASvsVPvsCS(filename):
     f = open(filename, 'r')
 
     headers = f.readline()
+
     xvals = [0]
     c10yvals = [0.0]
     c5yvals = [0.0]
@@ -51,16 +53,18 @@ def ASvsVPvsCS(filename):
         c2yvals.insert(i,float(arr[4]))
         c1yvals.insert(i,float(arr[2]))
 
-    plt.plot(xvals,c1yvals, label = "c1")
-    plt.plot(xvals,c2yvals, label = "c2")
-    plt.plot(xvals,c4yvals, label = "c4")
-    plt.plot(xvals,c5yvals, label = "c5")
-    plt.plot(xvals,c10yvals, label = "c10")
+    curFig = plt.figure()
+    plt.plot(xvals,c1yvals, label = "c1", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c2yvals, label = "c2", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c4yvals, label = "c4", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c5yvals, label = "c5", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c10yvals, label = "c10", figure=curFig, lw = 5.0)
     
     plt.legend(bbox_to_anchor=(0.,1.02,1.,.102), loc=2, ncol=5, mode = "expand", borderaxespad=0)
     plt.xlabel("Vantage Points")
     plt.ylabel("Anonymity Set Size")
-    plt.savefig("ASvsVPvsCS.png")
+    plt.yscale('log')
+    curFig.savefig("../plots/ASvsVPvsCS.pdf", format="pdf")
     
 def TPvsVPvsCS(filename):
     f = open(filename, 'r')
@@ -72,7 +76,8 @@ def TPvsVPvsCS(filename):
     c4yvals = [0.0]
     c2yvals = [0.0]
     c1yvals = [0.0]
-    
+
+    curFig = plt.figure()
     for line in f:
         arr = line.split(',')
         i = put(xvals,int(arr[0]))
@@ -82,14 +87,14 @@ def TPvsVPvsCS(filename):
         c2yvals.insert(i,float(arr[5]))
         c1yvals.insert(i,float(arr[3]))
         
-    plt.plot(xvals,c1yvals, label = "c1")
-    plt.plot(xvals,c2yvals, label = "c2")
-    plt.plot(xvals,c4yvals, label = "c4")
-    plt.plot(xvals,c5yvals, label = "c5")
-    plt.plot(xvals,c10yvals, label = "c10")
+    plt.plot(xvals,c1yvals, label = "c1", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c2yvals, label = "c2", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c4yvals, label = "c4", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c5yvals, label = "c5", figure=curFig, lw = 5.0)
+    plt.plot(xvals,c10yvals, label = "c10", figure=curFig, lw = 5.0)
 
     plt.legend(bbox_to_anchor=(0.,1.02,1.,.102), loc=2, ncol=5, mode = "expand", borderaxespad=0)
     plt.xlabel("Vantage Points")
     plt.ylabel("True Positive Rate")
-    plt.savefig("TPvsVPvsCS.png")
+    curFig.savefig("../plots/TPvsVPvsCS.pdf", format="pdf")
 
